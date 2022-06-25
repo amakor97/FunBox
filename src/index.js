b3eb3e9523335578d1ac43f76d11cab8541b9bc2
amakor97 <amakor.andrei@gmail.com>
2022-06-25 19:30:34 +0300

    delete unused files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import Offer from "./blocks/Offer/Offer";
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Offer />
 );
-
-reportWebVitals();
